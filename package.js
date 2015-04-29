3a1a168d2ad4b773b76f1acbb716e0eb5be789a5
Ben Jones <benjaminpeterjonesesquire@gmail.com>
2015-04-29 10:17:09 +0100

    Fix package.js

--- package.js
+++ package.js
@@ -2,9 +2,9 @@ Package.describe({
   name: 'yogiben:spinkit',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'Adds the SpinKit CSS loader library',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/yogiben/meteor-spinkit',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
